af2a61c94e74c0264246d2e7d081aedb0bd2c867
BorislavN <borko_nik@abv.bg>
2023-08-07 17:59:51 +0300

    Prepared folder structure

---
+++ src/iterators_and_comperators/Main.java
@@ -0,0 +1,13 @@
+package iterators_and_comperators;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
