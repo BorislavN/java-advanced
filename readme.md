5e3c5f4806358d8143ee3f9cc97c4fad4588c875
BorislavN <borko_nik@abv.bg>
2023-08-08 15:28:47 +0300

    Changed readme.md

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 Hello.
-Here you will find my solutions to the SoftUni Java-Advanced exercises.
+Here you will find my solutions to the SoftUni Java Advanced exercises.
 To test the code or view the exercise descriptions, login with your SoftUni account.
 
 Course page - https://judge.softuni.org/Contests/#!/List/ByCategory/175/Java-Advanced-Exercises
--- src/sets_and_maps/Main.java
+++ src/sets_and_maps/Main.java
@@ -3,11 +3,10 @@ package sets_and_maps;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.SecureClassLoader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-
     }
 }
