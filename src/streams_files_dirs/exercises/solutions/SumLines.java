2253f652120240c998294387d21d3b9d977e10fa
BorislavN <borko_nik@abv.bg>
2023-08-28 21:22:01 +0300

    Starting the streams_files_dirs exercises

---
+++ src/streams_files_dirs/exercises/solutions/SumLines.java
@@ -0,0 +1,25 @@
+package streams_files_dirs.exercises.solutions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SumLines {
+    public static void main(String[] args) {
+        Path path = Path.of("src/streams_files_dirs/exercises/resources/input.txt");
+        StringBuilder output = new StringBuilder();
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String input;
+
+            while (null != (input = reader.readLine())) {
+                output.append(input.chars().sum()).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(output);
+    }
+}
