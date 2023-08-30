e48dc24d9dff329166f48aa7c8995f3e4cadcfac
BorislavN <borko_nik@abv.bg>
2023-08-30 17:39:48 +0300

    Playing around with ExecutorService in MultithreadingExample.java

---
+++ src/streams_files_dirs/sandbox/MultithreadingExample.java
@@ -0,0 +1,85 @@
+package streams_files_dirs.sandbox;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MultithreadingExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String input = "Task1\nTask2\nTask3\nTask4\nTask5\nTask6\nTask7\nTask8\nTask9\nTask10";
+
+        singleThreadExample(input);
+        multiThreadExample(input);
+    }
+
+    private static void singleThreadExample(String input) throws IOException, InterruptedException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+
+        String line;
+        Instant start = Instant.now();
+
+        while (null != (line = reader.readLine())) {
+            Thread.sleep(500);//Simulating some work
+            System.out.println(line + ": COMPLETED!");
+        }
+
+        Instant end = Instant.now();
+
+        System.out.printf("%nSingleThreadExample: finished in %dms%n%n", Duration.between(start, end).toMillis());
+    }
+
+    private static void multiThreadExample(String input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        String line;
+        Instant start = Instant.now();
+
+        while (null != (line = reader.readLine())) {
+            executorService.submit(lineHandler(line));
+        }
+
+        shutdownAndAwaitTermination(executorService);
+
+        Instant end = Instant.now();
+
+        System.out.printf("%nMultiThreadExample: finished in %dms%n%n", Duration.between(start, end).toMillis());
+    }
+
+    private static Runnable lineHandler(String line) {
+        return () -> {
+            try {
+                Thread.sleep(500);//Simulating some work
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(line + ": COMPLETED!");
+        };
+    }
+
+    private static void shutdownAndAwaitTermination(ExecutorService pool) {
+        pool.shutdown(); // Disable new tasks from being submitted
+
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+                pool.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!pool.awaitTermination(10, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            pool.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+    }
+}
--- src/streams_files_dirs/sandbox/WritersSpeedComparison.java
+++ src/streams_files_dirs/sandbox/WritersSpeedComparison.java
@@ -9,11 +9,14 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
+import java.util.Scanner;
 
 //Speed comparison between different writers
 //On average the BufferedWriter is fastest
 public class WritersSpeedComparison {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Path currentLectureFolder = Path.of("src/streams_files_dirs/exercises");
 
         Path inputPath = currentLectureFolder.resolve("resources/testInput.txt");
@@ -21,59 +24,63 @@ public class WritersSpeedComparison {
         Path outputPathTwo = currentLectureFolder.resolve("resources/outputTwo.txt");
         Path outputPathThree = currentLectureFolder.resolve("resources/outputThree.txt");
 
-        StringBuilder output = new StringBuilder();
+        System.out.println("Enter number of loops:");
+        int times = Integer.parseInt(scanner.nextLine());
 
-        try (
-                PrintWriter printWriter = new PrintWriter(outputPathTwo.toFile());
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPathThree.toString()));
-                FileWriter fileWriter = new FileWriter(outputPathOne.toFile())
-        ) {
-            List<String> lines = Files.readAllLines(inputPath);
+        double fileWriterTotal = 0;
+        double printWriterTotal = 0;
+        double bufferedWriterTotal = 0;
 
-            //FileWriter
-            Instant start = Instant.now();
+        for (int loop = 0; loop < times; loop++) {
+            try (
+                    PrintWriter printWriter = new PrintWriter(outputPathTwo.toFile());
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPathThree.toString()));
+                    FileWriter fileWriter = new FileWriter(outputPathOne.toFile())
+            ) {
+                List<String> lines = Files.readAllLines(inputPath);
 
-            for (String line : lines) {
-                fileWriter.write(line.toUpperCase());
-                fileWriter.append(System.lineSeparator());
+                //FileWriter
+                Instant start = Instant.now();
+
+                for (String line : lines) {
+                    fileWriter.write(line.toUpperCase() + " - WORK");
+                    fileWriter.append(System.lineSeparator());
+                }
+
+                Instant end = Instant.now();
+
+                fileWriterTotal += Duration.between(start, end).toMillis();
+
+                //PrintWriter
+                start = Instant.now();
+
+                for (String s : lines) {
+                    printWriter.println(s.toUpperCase() + " - WORK");
+                }
+
+                end = Instant.now();
+
+                printWriterTotal += Duration.between(start, end).toMillis();
+
+                //BufferedWriter
+                start = Instant.now();
+
+                for (String line : lines) {
+                    bufferedWriter.write(line.toUpperCase() + " - WORK");
+                    bufferedWriter.newLine();
+                }
+
+                end = Instant.now();
+
+                bufferedWriterTotal += Duration.between(start, end).toMillis();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-
-            fileWriter.flush();
-            Instant end = Instant.now();
-
-            output.append(String.format("FileWriter: %d%n%n", Duration.between(start, end).toMillis()));
-            Thread.sleep(3000);
-
-            //PrintWriter
-            start = Instant.now();
-
-            for (String s : lines) {
-                printWriter.println(s.toUpperCase());
-            }
-
-            printWriter.flush();
-            end = Instant.now();
-
-            output.append(String.format("PrintWriter: %d%n%n", Duration.between(start, end).toMillis()));
-            Thread.sleep(3000);
-
-            //BufferedWriter
-            start = Instant.now();
-
-            for (String line : lines) {
-                bufferedWriter.write(line.toUpperCase());
-                bufferedWriter.newLine();
-            }
-
-            bufferedWriter.flush();
-            end = Instant.now();
-
-            output.append(String.format("BufferedWriter: %d%n", Duration.between(start, end).toMillis()));
-
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
         }
 
-        System.out.println(output);
+        System.out.printf("FileWriter: %.2f%n%n", fileWriterTotal / times);
+        System.out.printf("PrintWriter: %.2f%n%n", printWriterTotal / times);
+        System.out.printf("BufferedWriter: %.2f%n", bufferedWriterTotal / times);
     }
 }
