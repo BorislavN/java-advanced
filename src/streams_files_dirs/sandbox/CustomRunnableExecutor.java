2b6d8e0061b4ef29b0f3fe6fe61133d220df44ce
BorislavN <borko_nik@abv.bg>
2023-09-08 21:57:50 +0300

    Modified CustomRunnableExecutor.java, so the execution of the Runnable starts when the Task is instantiated.

--- src/streams_files_dirs/sandbox/CustomRunnableExecutor.java
+++ src/streams_files_dirs/sandbox/CustomRunnableExecutor.java
@@ -1,13 +1,12 @@
 package streams_files_dirs.sandbox;
 
-//Here i will try to implement something resembling Future
-//I wrapped up the Runnable run() method
-//And now I'm able to catch the exceptions thrown in different threads
+//Here I will try to implement something resembling Future
+//Currently the code successfully catches  the exceptions thrown in different threads
 public class CustomRunnableExecutor {
     public static void main(String[] args) {
         Task task1 = new Task(() -> {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
@@ -17,7 +16,7 @@ public class CustomRunnableExecutor {
 
         Task task2 = new Task(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1500);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
@@ -36,27 +35,29 @@ public class CustomRunnableExecutor {
 
 class Task {
     private final Object lock;
-    private final Runnable runnable;
     private final ExceptionHandler handler;
+    private final Thread worker;
 
     Task(Runnable runnable) {
         this.lock = new Object();
-        this.runnable = runnable;
         this.handler = new ExceptionHandler(this.lock);
+        this.worker = new Thread(new ExtendedRunnable(runnable, this.lock));
+        worker.setUncaughtExceptionHandler(this.handler);
+
+        this.start();
+    }
+
+    //starts the thread when the class is initialized
+    private void start() {
+        worker.start();
     }
 
     //Blocks until the result is ready, just like Future
+    //Task will start executing by default
+    //But if we don't call get() - we wont propagate the exceptions to the main thread
     public void get() throws InterruptedException, IllegalStateException {
         synchronized (this.lock) {
-            Thread worker = new Thread(new ExtendedRunnable(this.runnable, this.lock));
-            worker.setUncaughtExceptionHandler(this.handler);
-            worker.start();
-
-            //Even if the thread wakes up spontaneously
-            //We wait for the thread to exit
-            //So we don't miss an error, if thrown
-            this.lock.wait();
-
+            //Wait for the thread to stop
             while (worker.isAlive()) {
                 this.lock.wait(500);
             }
@@ -70,16 +71,10 @@ class Task {
 }
 
 record ExtendedRunnable(Runnable runnable, Object lock) implements Runnable {
-
     @Override
     public void run() {
-        synchronized (this.lock) {
-            this.runnable.run();
-
-            //we skip the notify call in case of error
-            //we call it in the error handler
-            this.lock.notifyAll();
-        }
+        this.runnable.run();
+        Thread.currentThread().interrupt();
     }
 }
 
@@ -92,15 +87,10 @@ class ExceptionHandler implements Thread.UncaughtExceptionHandler {
         this.exception = null;
     }
 
-    //we set the error
-    //and call notify, because we skipped it the run()
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        synchronized (this.lock) {
-            this.exception = e;
-            this.lock.notifyAll();
-            t.interrupt();//the thread should be closing, but we interrupt it just in case
-        }
+        this.exception = e;
+        t.interrupt();//the thread should be closing, but we interrupt it just in case
     }
 
     public Throwable getException() {
