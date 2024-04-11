ccdde7617cc8fa2eda1655eb24900d03cc14f1fd
BorislavN <borko_nik@abv.bg>
2024-04-11 23:27:51 +0300

    Solved one more, 1 left...

--- src/defining_classes/eight/Main.java
+++ src/defining_classes/eight/Main.java
@@ -3,11 +3,65 @@ package defining_classes.eight;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiPredicate;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String personToken = reader.readLine();
+        Person person = null;
 
+        List<String> relations = new ArrayList<>();
+        List<Person> relatives = new ArrayList<>();
+
+        String input;
+
+        while (!"End".equals(input = reader.readLine())) {
+            if (!input.contains("-")) {
+                String[] data = input.split("\\s+");
+
+                Person current = new Person(data[0], data[1], data[2]);
+
+                if (current.toString().contains(personToken)) {
+                    person = current;
+                }
+
+                relatives.add(current);
+
+                continue;
+            }
+
+            relations.add(input);
+        }
+
+        if (person == null) {
+            return;
+        }
+
+        for (String relation : relations) {
+            String[] data = relation.split("\\s+-\\s+");
+
+            BiPredicate<Person, String> filter = (p, token) -> p.toString().contains(token);
+
+            Person parent = relatives.stream()
+                    .filter(r -> filter.test(r, data[0]))
+                    .findAny()
+                    .orElse(null);
+
+            Person child = relatives.stream()
+                    .filter(r -> filter.test(r, data[1]))
+                    .findAny()
+                    .orElse(null);
+
+            if (parent != null && child != null) {
+                parent.addChild(child);
+                child.addParent(parent);
+            }
+        }
+
+        System.out.println(Person.getTree(person));
     }
 }
---
+++ src/defining_classes/eight/Person.java
@@ -0,0 +1,52 @@
+package defining_classes.eight;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate birthday;
+    private final List<Person> parents;
+    private final List<Person> children;
+
+    public Person(String firstName, String lastName, String date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = LocalDate.parse(date, DateTimeFormatter.ofPattern("d/M/yyyy"));
+        this.parents = new ArrayList<>();
+        this.children = new ArrayList<>();
+    }
+
+    public void addParent(Person parent) {
+        this.parents.add(parent);
+    }
+
+    public void addChild(Person child) {
+        this.children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s"
+                , this.firstName
+                , this.lastName
+                , this.birthday.format(DateTimeFormatter.ofPattern("d/M/yyyy")));
+    }
+
+    public static String getTree(Person person) {
+        StringBuilder output = new StringBuilder();
+
+        output.append(person).append(System.lineSeparator());
+
+        output.append("Parents:").append(System.lineSeparator());
+        person.parents.forEach(e -> output.append(e).append(System.lineSeparator()));
+
+        output.append("Children:").append(System.lineSeparator());
+        person.children.forEach(e -> output.append(e).append(System.lineSeparator()));
+
+        return output.toString();
+    }
+}
